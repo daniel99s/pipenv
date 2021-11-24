a4a44d3cab011a810c914b46a092cdfcc24f9311
Luis Soto <luis.daniel.soto.escalante@uabc.edu.mx>
2021-11-23 16:33:56 -0800

    commit del  pipenv

---
+++ main.py
@@ -0,0 +1,11 @@
+from colorama import Fore, Back, Style
+from art import *
+
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
+
+
+print(art("random"))
